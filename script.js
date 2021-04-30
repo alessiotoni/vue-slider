d06a0b1b34fc3dd59ea52959081178a8381b877a
alessiotoni <toniellialessio@gmail.com>
2021-04-30 18:15:07 +0200

    termina il progetto

--- script.js
+++ script.js
@@ -2,47 +2,50 @@ const vue = new Vue({
     el: "#root",
 
     data: {
-        img: {
-            name: "inverno",
-            urlImg: "https://www.freeage.it/wp-content/uploads/2020/05/armocromia-inverno.jpg"
-        },
+        imgIndex: 0,
+
         imgList: [
             {
-                name: "inverno",
+                title: "inverno",
                 urlImg: "https://www.freeage.it/wp-content/uploads/2020/05/armocromia-inverno.jpg"
             },
             {
-                name: "primavera",
+                title: "primavera",
                 urlImg: "https://static2-viaggi.corriereobjects.it/wp-content/uploads/2021/03/iStock-1128908941.jpg?v=427355"
             },
             {
-                name: "estate",
+                title: "estate",
                 urlImg: "https://studioidentity.net/wp-content/uploads/2017/07/6-curiosita%CC%80-sullestate_FB.jpg"
             },
             {
-                name: "autunno",
-                urlImg: "https://frasisocial.com/wp-content/uploads/2018/12/autunno.jpg"
+                title: "autunno",
+                urlImg: "https://www.occhionotizie.it/wp-content/uploads/2020/09/111354497-edcba630-d5e8-4517-9fe1-8c103e1f11e3.jpg"
             }
         ]
     },
+
     methods: {
-        changeImg: function () {
-            // if (this.img.name == "inverno"){
-            //     this.img.name = "primavera"
-            //     this.img.urlImg = "https://static2-viaggi.corriereobjects.it/wp-content/uploads/2021/03/iStock-1128908941.jpg?v=427355"
-            // }
 
-            this.imgList.forEach((element,i) => {
-                if (element == this.img) {
-                    this.img = {...element[(i + 1)]}
-                }
+        nextImg: function () {
+            this.imgIndex++;
 
-            });
+            if (this.imgIndex == this.imgList.length) {
+                this.imgIndex = 0;
+            };
+        },
 
+        previousImg: function () {
+            if (this.imgIndex > 0) {
+                this.imgIndex--;;
+            };
 
+        },
 
-            
-        }
-    }
+        jumpImg: function (index) {
+            console.log(index)
+            this.imgIndex = index
+            // index == imgList? 'true' : 'false';
+        },
+    },
 
 });
